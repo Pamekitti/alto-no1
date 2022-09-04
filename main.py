b34054832fa21c3d8c5fb50e1646864fbed0e2e0
Pamekitti <pamekitti.p@gmail.com>
2022-09-05 00:56:30 +0700

    Add Line Week Chart
    The plot uses a function from plot_error_mode.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import plotly.express as px
 import pandas as pd                        # pip install pandas
 import numpy as np
 from process_thai import thai_province_map, thai_datetime
+from plot_error_mode import plot_line_error
 
 # Get Thailand map JSON File
 import urllib.request, json
@@ -19,7 +20,6 @@ id_main = id_table.drop_duplicates(subset=['id'], keep="first", inplace=False)
 # Merge Names to match JSON File
 df = thai_province_map(id_main, json_map)
 # Merge info from main stations
-current_time = pd.to_datetime('202204' '09 13:00')
 to_merge = []
 for station_id in df['station_id']:
     if np.isnan(station_id):
@@ -29,22 +29,30 @@ for station_id in df['station_id']:
     df_main.drop(columns=df_main.columns[0], axis=1, inplace=True)
     df_main['datetime'] = df_main['datetime'].apply(lambda var: thai_datetime(var))
     df_main['station_id'] = int(station_id)
-    try:
-        # current_time = df_main.iloc[0,0]
-        current_stat = df_main[df_main['datetime'] == current_time]
-        to_merge.append(current_stat.values.tolist()[0])
-        print(current_time)
-    except:
-        current_stat = df_main.iloc[0]
-        print ('except')
-        # to_merge.append(current_stat.values.tolist())
+    to_merge += df_main.values.tolist()
 df_to_merge = pd.DataFrame(to_merge, columns=['datetime', 'temp', 'dew_point', 'rh',
                                               'pressure', 'wind_dir', 'wind_sp',
                                               'visibility', 'rain_3h', 'cloud', 'station_id'])
-df = df.merge(df_to_merge, how='left', on='station_id')
+df_day = df.merge(df_to_merge, how='left', on='station_id')
 
+to_merge = []
+for station_id in df['station_id']:
+    if np.isnan(station_id):
+        print(f'Station id: {station_id} not found')
+        continue
+    df_main = pd.read_csv(f'week_csv/week_{int(station_id)}.csv')
+    df_main.drop(columns=df_main.columns[0], axis=1, inplace=True)
+    df_main['date'] = df_main['date'].apply(lambda var: thai_datetime(var, day_data=False))
+    df_main['station_id'] = int(station_id)
+    to_merge += df_main.values.tolist()
+df_to_merge = pd.DataFrame(to_merge, columns=['date', 'min_temp', 'max_temp', 'max_wind_dir',
+                                              'max_wind_sp', 'rain_24h', 'note', 'station_id'])
+df_week = df.merge(df_to_merge, how='left', on='station_id')
+
+'''
+DASH Web App
+'''
 app = dash.Dash(__name__)
-
 app.layout = html.Div([
     html.Div(children=[
         html.Button('Add Chart', id='add-chart', n_clicks=0)
@@ -75,8 +83,10 @@ def display_graphs(n_clicks, div_children):
                     'index': n_clicks
                 },
                 options=[{'label': 'Choropleth Map', 'value': 'choropleth'},
-                         {'label': 'Bar Chart', 'value': 'bar'}],
-                value='bar',
+                         {'label': 'Bar Chart', 'value': 'bar'},
+                         {'label': 'Line Day Chart', 'value': 'line_day'},
+                         {'label': 'Line Week Chart', 'value': 'line_week'}],
+                value='choropleth',
             ),
             dcc.Dropdown(
                 id={
@@ -85,7 +95,7 @@ def display_graphs(n_clicks, div_children):
                 },
                 options=[{'label': s, 'value': s} for s in np.sort(df['eng_province'].unique())],
                 multi=True,
-                value=["Bangkok Metropolis", "Rayong"],
+                value=["Bangkok Metropolis", "Rayong", "Chiang Mai"],
             ),
             dcc.Dropdown(
                 id={
@@ -111,21 +121,36 @@ Output({'type': 'dynamic-graph', 'index': MATCH}, 'figure'),
 )
 def update_graph(prov_value, val_value, chart_choice):
     print(prov_value)
-    dff = df[df['eng_province'].isin(prov_value)]
+    current_time = thai_datetime('04 ก.ย. 65 13:00 น.')
+
+    dff_day = df_day[df_day['eng_province'].isin(prov_value)]
+    dff_now = dff_day[dff_day['datetime'] == current_time]
+
+    dff_week = df_week[df_week['eng_province'].isin(prov_value)]
 
     if chart_choice == 'choropleth':
-        fig = px.choropleth_mapbox(df, geojson=json_map, color=val_value,
+        fig = px.choropleth_mapbox(df_day, geojson=json_map, color=val_value,
                                    locations="eng_province", featureidkey="properties.name",
                                    center={"lat": 13.736717, "lon": 100.523186},
                                    mapbox_style="open-street-map", zoom=4)
         fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
         return fig
     elif chart_choice == 'bar':
-        fig = px.bar(dff, x='eng_province', y=val_value,
+        fig = px.bar(dff_now, x='eng_province', y=val_value,
                      hover_data=['temp', 'rh', 'pressure', 'rain_3h'], color='temp',
                      labels={'eng_province': 'Province'}, height=400)
+        return fig
+    elif chart_choice == 'line_day':
+        fig = px.line(dff_day, x="datetime", y=val_value, color='eng_province')
+        return fig
+    elif chart_choice == 'line_week':
+        dff_week['temp_error'] = (dff_week['max_temp'] - dff_week['min_temp']) / 2
+        dff_week['mean_temp'] = dff_week['min_temp'] + dff_week['temp_error']
+        fig = plot_line_error(error_y_mode='band', data_frame=dff_week, x='date',
+                              y='mean_temp', error_y='temp_error', color='eng_province')
+        # fig = px.line(dff_week, x="date", y='mean_temp', color='eng_province')
         return fig
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8000)
+    app.run_server(debug=False, port=8000)
