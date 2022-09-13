7e5b0907a8e46c88dead9a0317305552b002a128
Pamekitti Puktalae <pamekitti.p@gmail.com>
2022-09-13 19:27:57 +0700

    move-to-work-in-mac

---
+++ app.py
@@ -0,0 +1,5 @@
+import dash
+import os
+app_port = 80 # os.environ['APP_PORT']
+
+app = dash.Dash(__name__)
---
+++ overview.py
@@ -0,0 +1,9 @@
+import dash
+from dash import html, dcc
+import dash_daq as daq
+from dash.dependencies import Input, Output, State
+
+app = dash.Dash(__name__)
+app.layout = html.Div([
+    html.Div([])
+])
